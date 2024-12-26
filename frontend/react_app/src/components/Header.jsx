9ec8a896b82a9b28306f073f3e4fb5639ca86feb
lakitha21 <lakithavirajuni@gmail.com>
2024-12-27 00:02:06 +0530

    Onboarding
    created onboarding screen

---
+++ frontend/react_app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default function Header() {
+  return (
+    <div>
+        <img src="D:\GUI_Project\BudgetLense\frontend\react_app\public\image.jpg"/>
+    </div>
+  )
+}
+
