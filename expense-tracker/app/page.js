9ea86ba4ae14f14fb4cd2ba6c230873226ed9f22
lakitha21 <lakithavirajuni@gmail.com>
2024-12-30 10:44:30 +0530

    onboarding final
    Final edit of the onboarding screen

--- expense-tracker/app/page.js
+++ expense-tracker/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Header from "./_component/Header";
+import Hero from "./_component/Hero";
 
 
 export default function Home() {
   return (
     <div>
       <Header/>
+      <Hero/>
     </div>
   );
 }
